655c25544e633816ba1980bb075c5795dfdda82f
zomby7e <zomby7e@gmail.com>
2022-11-07 09:18:56 +0800

    First run

---
+++ js/read.17cc6ba2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk_7eblog_frontend"]=self["webpackChunk_7eblog_frontend"]||[]).push([[886],{6240:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(3396);function u(e,n,r,u,o,d){const i=(0,t.up)("MarkdownArticle");return(0,t.wg)(),(0,t.iD)("div",null,[(0,t.Wm)(i,{id:e.$route.query.id,type:e.$route.query.type},null,8,["id","type"])])}var o=r(2115),d=(0,t.aZ)({name:"ReadView",components:{MarkdownArticle:o.Z}}),i=r(89);const a=(0,i.Z)(d,[["render",u]]);var c=a}}]);
+//# sourceMappingURL=read.17cc6ba2.js.map
